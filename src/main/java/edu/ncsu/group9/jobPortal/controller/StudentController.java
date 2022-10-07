b79ff1df83107fbd6a9a847c8a4c7bfc2ea3f8a0
tilaksatra <ayushsatra@gmail.com>
2022-10-07 23:31:23 +0530

    Added Documentation for Controllers

--- src/main/java/edu/ncsu/group9/jobPortal/controller/StudentController.java
+++ src/main/java/edu/ncsu/group9/jobPortal/controller/StudentController.java
@@ -1,8 +1,10 @@
 package edu.ncsu.group9.jobPortal.controller;
 
 import edu.ncsu.group9.jobPortal.dao.UserDao;
+import edu.ncsu.group9.jobPortal.model.CompanyInfo;
 import edu.ncsu.group9.jobPortal.model.Student;
 import edu.ncsu.group9.jobPortal.service.StudentService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,8 @@ public class StudentController {
     UserDao userDao;
 
     @PostMapping(path = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Registers and Adds the student to the portal",
+            notes = "Requires Student Information")
     public void addStudent(@RequestBody Student student) {
         studentService.addStudent(student);
         userDao.addUser(student);
