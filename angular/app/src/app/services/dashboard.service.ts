5653fb451b649a6a936eabbc6e2b6f69bf24234a
tilaksatra <ayushsatra@gmail.com>
2022-10-07 06:58:33 +0530

    fixing student and coordinator distinction

--- angular/app/src/app/services/dashboard.service.ts
+++ angular/app/src/app/services/dashboard.service.ts
@@ -8,8 +8,8 @@ export default class DashboardService {
     private url: string;
     constructor(private http: HttpClient) {
         this.url =
-            // 'http://localhost:8085/dashboard/get/all';
-            'https://run.mocky.io/v3/3e635cbe-68cc-4cf4-8c33-59babd753437';
+            'http://localhost:8085/dashboard/get/all';
+            //'https://run.mocky.io/v3/3e635cbe-68cc-4cf4-8c33-59babd753437';
     }
 
     public getCompanies() {
--- src/main/java/edu/ncsu/group9/jobPortal/dao/UserDao.java
+++ src/main/java/edu/ncsu/group9/jobPortal/dao/UserDao.java
@@ -2,8 +2,10 @@ package edu.ncsu.group9.jobPortal.dao;
 
 import edu.ncsu.group9.jobPortal.model.Student;
 import edu.ncsu.group9.jobPortal.model.User;
+import edu.ncsu.group9.jobPortal.model.UserDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -18,29 +20,37 @@ public class UserDao {
 
     public static final String CHECK_USER_SQL = "SELECT userId, userPassword FROM JOBPORTAL.USER WHERE USERID=?";
 
-    public static final String INSERT_USER_SQL = "INSERT INTO JOBPORTAL.USER VALUES (?, ?)";
+    public static final String INSERT_USER_SQL = "INSERT INTO JOBPORTAL.USER VALUES (?, ?, ?)";
 
-    public static final String SUCCESSFUL = "LOGIN SUCCESSFUL";
+    public static final String COORDINATOR_LOGIN_SUCCESSFUL = "COORDINATOR LOGIN SUCCESSFUL";
+    public static final String STUDENT_LOGIN_SUCCESSFUL = "STUDENT LOGIN SUCCESSFUL";
     public static final String UNSUCCESSFUL = "LOGIN UNSUCCESSFUL";
 
 
     public String checkUserRecord(User user) {
         String userId = user.getUserId();
-        String userPassword = user.getUserPassword();
-        User fetchedUser = null;
+        UserDto fetchedUser = null;
         try {
-            fetchedUser = jdbcTemplate.queryForObject(CHECK_USER_SQL, new Object[] { userId }, new BeanPropertyRowMapper<>(User.class));
-            return Objects.isNull(fetchedUser) ? UNSUCCESSFUL : (user.getUserPassword().equals(fetchedUser.getUserPassword())? SUCCESSFUL : UNSUCCESSFUL);
+            fetchedUser = jdbcTemplate.queryForObject(CHECK_USER_SQL, new Object[] { userId }, new BeanPropertyRowMapper<>(UserDto.class));
+            if (fetchedUser!=null && fetchedUser.getIsStudent() == 1)
+                checkStudentCredentials(user, fetchedUser);
+            return Objects.isNull(fetchedUser) ? UNSUCCESSFUL : (user.getUserPassword().equals(fetchedUser.getUserPassword())? COORDINATOR_LOGIN_SUCCESSFUL : UNSUCCESSFUL);
+        } catch (EmptyResultDataAccessException exception) {
+            log.warn("No user found with userId -> {}", user.getUserId());
         } catch (Exception exception) {
             exception.printStackTrace();
         }
         return UNSUCCESSFUL;
     }
 
+    public String checkStudentCredentials(User user, UserDto fetchedUser) {
+        return user.getUserPassword().equals(fetchedUser.getUserPassword()) ? STUDENT_LOGIN_SUCCESSFUL: UNSUCCESSFUL;
+    }
+
     public int addUser(Student student) {
         int rows = 0;
         try {
-            rows = jdbcTemplate.update(INSERT_USER_SQL, student.getEmailId(), student.getPassword());
+            rows = jdbcTemplate.update(INSERT_USER_SQL, student.getEmailId(), student.getPassword(), 1);
             log.info("Inserted {} rows(s) into the User Table for {}", rows, student.getEmailId());
         } catch (Exception exception) {
             log.error("Error Occurred while inserting record in the user table. Exception -> ");
